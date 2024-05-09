2df272f06623b5ddd82fb4098e5bb3014b4bb59a
MuesliW <128370392+MuesliW@users.noreply.github.com>
2024-05-09 10:07:26 +0800

    update ip to 0.0.0.0 to be accessible for docker process

--- example/callback-server-go/main.go
+++ example/callback-server-go/main.go
@@ -5,7 +5,7 @@ import "github.com/cobo/cobo-mpc-callback-server-examples/example/callback-serve
 func main() {
 	config := &service.CallBackConfig{
 		ServiceName:               "TEST-001",
-		Endpoint:                  "localhost:11020",
+		Endpoint:                  "0.0.0.0:11020",
 		TokenExpireMinutes:        2,
 		CallBackPubKeyPath:        "./cobo-tss-node-risk-control-pub.key",   // TSS Node's pub key to verify token received
 		CallBackHandlerPriKeyPath: "./callback-server-pri.pem", // local pri key to generate token
